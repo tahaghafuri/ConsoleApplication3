36e2d58d5c4a5a15f2e4cd2f2d9b64985a4851c0
Taha Amin Ghafuri <tahaghafuri@gmail.com>
2022-10-11 22:52:14 +0330

    first commit

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = {"Taha","Motahareh","Mahyar"};
+
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
